f727538fb4b6c125df7772fc52657f652b624802
Jay Mundrawala <jdmundrawala@gmail.com>
2014-09-21 20:24:08 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in busser-pester.gemspec
+gemspec
---
+++ lib/busser/pester.rb
@@ -0,0 +1,7 @@
+require "busser/pester/version"
+
+module Busser
+  module Pester
+    # Your code goes here...
+  end
+end
