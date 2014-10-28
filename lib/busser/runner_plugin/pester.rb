e4102fdeff9f3e08f19899b004beef4d18597d1c
Steven Murawski <steven.murawski@gmail.com>
2014-10-28 20:32:55 +0100

    add some additional spacing before the output of Invoke-Pester

--- lib/busser/runner_plugin/pester.rb
+++ lib/busser/runner_plugin/pester.rb
@@ -34,7 +34,7 @@ class Busser::RunnerPlugin::Pester < Busser::RunnerPlugin::Base
     banner "[pester] Running"
     pester_path = suite_path('pester').to_s
     Dir.chdir(pester_path) do
-      run!("powershell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command \"Import-Module Pester; Invoke-Pester -EnableExit\"")
+      run!("powershell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command \"Import-Module Pester; write-host ''; Invoke-Pester -EnableExit\"")
     end
   end
 end
